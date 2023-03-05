ce79377fdbbe3831f5363f60fcf3c8242d0e3d64
yandi <lintangyandinugraha@gmail.com>
2023-03-05 18:59:18 +0700

    Tugas Fragment PPAPB 2

---
+++ app/src/main/java/com/example/tugasfragment/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.tugasfragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+    }
+}
---
+++ app/src/main/java/com/example/tugasfragment/MainFragment.java
@@ -0,0 +1,29 @@
+package com.example.tugasfragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
