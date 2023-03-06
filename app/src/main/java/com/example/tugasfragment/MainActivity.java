af673d3da4940d800746b49623087d551036947d
yandi <lintangyandinugraha@gmail.com>
2023-03-06 13:06:09 +0700

    Fragment Dinamis

--- app/src/main/java/com/example/tugasfragment/MainActivity.java
+++ app/src/main/java/com/example/tugasfragment/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.tugasfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,12 +12,55 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private boolean isFragmentDisplayed = false;
+
+    private View mainFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mainFragment = findViewById(R.id.main_fragment);
+
         button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isFragmentDisplayed) {
+                    displayFragment();
+                } else {
+                    closeFragment();
+                }
+            }
+        });
+    }
+
+    public void displayFragment() {
+        DetailFragment detailFragment = DetailFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.fragment_detail, detailFragment).addToBackStack(null)
+                .commit();
+        button.setText("< Back");
+        isFragmentDisplayed = true;
+
+        mainFragment.setVisibility(View.INVISIBLE);
+    }
+
+    public void closeFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        DetailFragment detailFragment = (DetailFragment) fragmentManager
+                .findFragmentById(R.id.fragment_detail);
+
+        if (detailFragment != null) {
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(detailFragment).commit();
+        }
+        button.setText("Show more");
+        isFragmentDisplayed = false;
+
+        mainFragment.setVisibility(View.VISIBLE);
     }
 }
